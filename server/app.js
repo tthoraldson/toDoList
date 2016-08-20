1e0d2196d8e5ba3b0cb2e456961bc5405dec0a2c
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-08-20 16:10:45 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+var express =  require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Our routes
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Catchall route
+app.get('/*', function (req, res) {
+  var file = req.params[0] || '/views/index.html';
+  res.sendFile(path.join(__dirname, './public', file));
+});
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Listening on port ', app.get('port'));
+});
