2f866cdddf809d79834760facf7d4ef9d280d276
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-08-20 19:54:07 -0500

    added some bootstrap styling

--- server/public/scripts/clientside.js
+++ server/public/scripts/clientside.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-  console.log('document ready');
+
+  // COMPLETE CLICKED TASK
+  $('.uncompletedTasksContainer').on('click', '.glyphicon-ok', completeClickedTask);
 });
+
+// GET ALL TASKS
+function getAllTasks(){
+  
+}
+
+// GET ALL UNCOMPLETED TASKS FROM DATABASE
+function getUncompletedTasks(){
+
+}
+
+// GET ALL COMPLETED TASKS FROM DATABASE
+function getCompletedTasks(){
+
+}
+
+// COMPLETE CLICKED TASK - MOVE TO COMPLETED TASKS
+function completeClickedTask(){
+  console.log('starting completeClickedTask');
+}
+
+// DELETE CLICKED TASK - REMOVE FROM DATABASE
+function deleteClickedTask(){
+  console.log('starting deleteClickedTask');
+}
